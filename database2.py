9454ad568c66125cee9ccc4e1cdfdaf3c4b228e6
John <johnmpalmgren@gmail.com>
2020-07-17 16:28:42 +0100

    changed database to class

--- database2.py
+++ database2.py
@@ -1,45 +1,48 @@
 import sqlite3
 
-database = 'speech.sqlite'
 
-
-def context_manager(command):
-    """Creates a context manager for opening and closing the database
-    parameters:
-        command(str): SQL database command
-    """
+class Database():
     
-    with sqlite3.connect(database) as conn:
-        global cur
-        cur = conn.cursor()
-        return cur.execute(command)
-
-
-def get_political_names(query):
-    """get list of names of all parties or speakers
-    parameters:
-        query(str): 'party' or 'speaker'
-    """
+    def __init__(self, database_name):
+        self.database_name = database_name
         
-    output = []
-    context_manager(f'SELECT {query} FROM  Speeches')
-    for row in cur:
-        if row[0] in output:
-            continue
-        else:
-            output.append(row[0])
-    return output
+    def context_manager(self, command):
+        """Creates a context manager for opening and closing the database
+        command(str): SQL database command
+        """
+    
+        with sqlite3.connect(self.database_name) as conn:
+            self.cur = conn.cursor()
+            return self.cur.execute(command)
 
+    def get_political_names(self, column):
+        """Get list of names of all parties or speakers
+        column(str): Select the column in the database. Enter either 'party'
+         or 'speaker
+        """
+        
+        output = []
+        self.context_manager(f'SELECT {column} FROM  Speeches')
+        for row in self.cur:
+            if row[0] in output:
+                continue
+            else:
+                output.append(row[0])
+        return output
 
-def get_speech(political_member, column):
-    """Return a list of speeches which match speaker/party
-    Parameters:
+    def get_speech(self, political_member, column):
+        """Return a list of speeches which match speaker/party
         political member (str): The name of the political party or speaker.
         column(str): Select the column in the database. Enter either 'party'
         or 'speaker'.
-    """
+        """
        
-    context_manager(f'SELECT speech FROM Speeches WHERE {column} = "{political_member}"')
+        self.context_manager(f'SELECT speech FROM Speeches WHERE {column} =' 
+                             f'"{political_member}"')
     
-    return [row for row in cur]
+        return [row for row in self.cur]        
     
+
+
+
+
